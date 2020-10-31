756e9a322d930c011cb9dcc1a83b0162fee935a3
MikeFogz <MikeFogz@gmail.com>
2020-10-31 16:19:07 -0700

    beginning primary function, getlocalstorage, for loop to check if time is current past future, event lsitener added and some tweaks to html (mostly making sure assests are linked properly)

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,31 @@ $(document).ready(function() {
     // let saveBtn = document.querySelector(".btnClass")
     let timeNow = parseInt(moment().format("H"))
 
+    //Primary Function. Begin with retreving local storage. 
+    function primaryFunction(){
+        getFromLocalStorage()
+
+        //add class "userInput" to text area div
+        $(currentHour).addClass("userInput")
+        let userInput = document.querySelector(".userInput")
+        timeDate.textContent = moment().format("dddd, MMMM Do YYYY");
+
+        //for loop to determine if area is present, future, or past in relation to current time
+        for(let i = 0; i < currentHour.length; i++) {
+            if(parseInt(currentHour[i].id.slice(4)) === timeNow) {
+                $(currentHour[i]).addClass("present")
+            } else if(parseInt(currentHour[i].id.slice(4)) > timeNow) {
+                $(currentHour[i]).addClass("future")
+            } else (parseInt(currentHour[i].id.slice(4)) < timeNow) {
+                $(currentHour[i]).addClass("past")
+            }
+        }
+        //adding event listener
+        var btnClick = document.querySelectorAll("button");
+        for (let i = 0; len = btnClick.length; i< len ; index++) {
+            // console.log(i)
+            btnClick[i].addEventListener("click", savetoLocalStorage);
+        }
+            
+        }
+    }
